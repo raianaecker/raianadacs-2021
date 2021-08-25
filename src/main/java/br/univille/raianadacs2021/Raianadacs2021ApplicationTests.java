84e7b22579764ac835f825f481eff22703f02827
Raiana Ecker <raianasafraider@hotmail.com>
2021-08-25 15:40:31 -0300

    atualizando

---
+++ src/main/java/br/univille/raianadacs2021/Raianadacs2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.raianadacs2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Raianadacs2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
