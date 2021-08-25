a4d47aa1b912526a6c89fa23b1e4fc315c4102f0
Raiana Ecker <raianasafraider@hotmail.com>
2021-08-25 16:27:34 -0300

    vinculo da classe categoria com a classe produto

--- src/main/java/br/univille/raianadacs2021/controller/ProdutoController.java
+++ src/main/java/br/univille/raianadacs2021/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package br.univille.raianadacs2021.controller;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.walterdacs2021.model.Categoria;
 import br.univille.raianadacs2021.model.Produto;
+import br.univille.walterdacs2021.service.CategoriaService;
 import br.univille.raianadacs2021.service.ProdutoService;
 
 @Controller
@@ -22,6 +25,8 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService service;
+    @Autowired
+    private CategoriaService categoriaService;
 
     /* 
     Date aniversario = new Date();
@@ -30,15 +35,33 @@ public class ProdutoController {
     Método - Função
     public tipo nome (parametros) { corpo } */
     
-    @GetMapping
+     @GetMapping
     public ModelAndView index(){
+        /*Produto prod1 = new Produto();
+        prod1.setDescricao("Produto legal 1");
+        prod1.setDataRegistro(new Date());
+        prod1.setPreco(2000);
+        Produto prod2 = new Produto();
+        prod2.setDescricao("Produto legal 2");
+        prod2.setDataRegistro(new Date());
+        prod2.setPreco(5000);*/
+
         List<Produto> listaProdutos = service.getAllProdutos();
+
+        /*listaProdutos.add(prod1);
+        listaProdutos.add(prod2);*/
 
         return new ModelAndView("produto/index","listaProdutos",listaProdutos);
     }
+    
     @GetMapping("/novo")
     public ModelAndView novo(@ModelAttribute Produto produto){
-        return new ModelAndView("produto/form");
+        HashMap<String,Object> dados = new HashMap<>();
+
+        dados.put("produto",produto);
+        List<Categoria> listaCategorias = categoriaService.getAllCategorias();
+        dados.put("listaCategorias",listaCategorias);
+        return new ModelAndView("produto/form",dados);
     }
 
     @PostMapping(params = "form")
@@ -49,7 +72,13 @@ public class ProdutoController {
 
     @GetMapping(value = "/alterar/{id}")
     public ModelAndView alterar(@PathVariable("id") Produto produto){
-        return new ModelAndView("produto/form","produto",produto);
+
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("produto",produto);
+        List<Categoria> listaCategorias = categoriaService.getAllCategorias();
+        dados.put("listaCategorias",listaCategorias);
+
+        return new ModelAndView("produto/form",dados);
     }
 
     @GetMapping(value = "/delete/{id}")
