8cdda22fd3df02c38003f05379463a1cdad5f093
Raiana Ecker <raianasafraider@hotmail.com>
2021-09-28 18:54:58 -0300

    correção

--- src/main/java/br/univille/raianadacs2021/service/ProdutoService.java
+++ src/main/java/br/univille/raianadacs2021/service/ProdutoService.java
@@ -9,4 +9,5 @@ public interface ProdutoService {
     public List<Produto> getAllProdutos();
     public Produto save(Produto produto);
     public void delete(Produto produto);
+    public List<Produto> importProduto(Fornecedor fornecedor);
 }
--- src/main/java/br/univille/raianadacs2021/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/univille/raianadacs2021/service/impl/ProdutoServiceImpl.java
@@ -3,9 +3,19 @@ package br.univille.raianadacs2021.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.univille.raianadacs2021.model.Fornecedor;
 import br.univille.raianadacs2021.model.Produto;
 import br.univille.raianadacs2021.repository.ProdutoRepository;
 import br.univille.raianadacs2021.service.ProdutoService;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 @Service
 public class ProdutoServiceImpl implements ProdutoService{
@@ -28,4 +38,39 @@ public class ProdutoServiceImpl implements ProdutoService{
         repository.delete(produto);
     }
     
+    @Override
+    public List<Produto> importProduto(Fornecedor fornecedor) {
+
+        if(fornecedor != null){
+            try {
+            URL endereco = new URL("http://1b22-186-237-248-5.ngrok.io/api/v1/produtos");
+            HttpURLConnection conn = (HttpURLConnection)endereco.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            
+            if(responseCode == 200){
+
+                Scanner leitor = new Scanner(endereco.openStream());
+
+                StringBuilder jsonText = new StringBuilder();
+                while(leitor.hasNext()){
+                    jsonText.append(leitor.nextLine());
+                }
+
+                Gson gson = new Gson();
+
+                Type typeListProdutos = new TypeToken<ArrayList<Produto>>(){}.getType();
+                ArrayList<Produto> listaProdutos = gson.fromJson(jsonText.toString(), typeListProdutos);
+                return listaProdutos;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+        return new ArrayList<Produto>()
+    }
 }
